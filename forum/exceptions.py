d245aa7167552358f27950f8b3b3144292d85cbf
itelman <ilyastelman02@gmail.com>
2025-04-03 14:58:30 +0500

    fix: exception handling

--- forum/exceptions.py
+++ forum/exceptions.py
@@ -5,7 +5,7 @@ from rest_framework.views import exception_handler
 def bad_request_handler(exc, context):
     response = exception_handler(exc, context)
 
-    if response.status_code == status.HTTP_400_BAD_REQUEST:
+    if response.status_code == status.HTTP_400_BAD_REQUEST and isinstance(response.data, dict):
         response.data = {
             key: value[0] if isinstance(value, list) and len(value) == 1 else value
             for key, value in response.data.items()
